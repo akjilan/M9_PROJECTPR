29640020cfd7099b07914c09a55592c9b3cdd5a1
akjilan <jilann.aj@gmail.com>
2023-10-22 15:34:11 +0600

    routes added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './components/Error/ErrorPage.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    children:[
+      {
+        path:"/about",
+        element:<div>jilan</div>
+  
+      }
+    ]
+  },
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage></ErrorPage>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
